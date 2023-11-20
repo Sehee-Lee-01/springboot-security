0bfe4a933a9aab6bc176a848a58f26f54c563990
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-20 16:43:51 +0900

    init: 프로그램 초기 설정

---
+++ src/test/java/com/sehee/weeklysecurity/WeeklysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehee.weeklysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklysecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
